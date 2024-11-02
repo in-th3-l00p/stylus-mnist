1b10bd7c9643347b0bbf4ee619a2e75d67f59d7f
in-th3-l00p <indianucode0303@gmail.com>
2024-11-02 20:04:31 +0200

    added project files

---
+++ mnist-draw/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import PixelGrid from './components/PixelGrid'
+
+function App() {
+  return (
+    <main className="w-screen min-h-screen flex justify-center items-center bg-zinc-700 gap-8">
+      <PixelGrid />
+    </main>
+  )
+}
+
+export default App
---
+++ mnist-draw/src/utils/predictInput.ts
@@ -0,0 +1,3 @@
+export default async function predictInput(input: number[][]): Promise<number> {
+    return 1;
+}
