60bad6e5ff8439bbb5e0d7e6f97c4f6e4066f12e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-11-03 00:00:20 +0200

    update lib.rs

--- classifier/src/lib.rs
+++ classifier/src/lib.rs
@@ -2,31 +2,18 @@
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
-use alloy_primitives::I16;
 /// Import items from the SDK. The prelude contains common traits and macros.
-use stylus_sdk::{alloy_primitives::U256, prelude::*, storage::StorageI16};
+use stylus_sdk::{console, prelude::*};
 
 // Define some persistent storage using the Solidity ABI.
-// `Counter` will be the entrypoint.
-sol_storage! {
-    #[entrypoint]
-    pub struct NeuralNet {
-    }
-}
-
+// `NeuralNet` will be the entrypoint.
 #[storage]
-pub struct Point {
-    top: StorageI16,
-    bot: StorageI16
-}
+#[entrypoint]
+struct NeuralNet;
 
-/// Declare that `Counter` is a contract with the following external methods.
 #[public]
 impl NeuralNet {
-    fn add(a: Point, b: Point) -> Point {
-        let top = a.top.checked_add(b.top);
-        let bota = a.bot;
-        let botb = b.bot;
-        let bot = bota.checked_add(botb.get()).unwrap();
+    fn user_main() -> u128 {
+        420
     }
 }
