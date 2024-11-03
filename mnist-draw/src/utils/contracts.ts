1755a4f06a2c944fc698ba58528f94ecba54109e
in-th3-l00p <indianucode0303@gmail.com>
2024-11-03 02:09:23 +0200

    nici numai stiu ce am facut

---
+++ mnist-draw/src/utils/contracts.ts
@@ -0,0 +1,9 @@
+// decapotabila cristina, asa am cumparat-o ca asa a vrut masina
+// hey masina, am cumparat cristina,
+// hai sa dam o tura sa-ti creasca adrenalina
+export const AUTHORIZATION="0x104f5cc5d1593f1ba2a0eecf5882be85e231aca9";
+export const ABI_TALENT=[
+  "function purchase() external payable returns (uint256)",
+  "function balanceOf(address _address) external view returns (uint256)",
+  "function markUsage(address _address) external returns (uint256)"
+];
